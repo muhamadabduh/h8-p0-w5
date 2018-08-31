1b3507db21d4edd0f60b918c030ff48d974a4ad7
DuhAbduh <muhamad.abduh.muh@gmail.com>
2018-08-31 14:08:27 +0700

    add exercise Regex

---
+++ exercise-8.js
@@ -0,0 +1,20 @@
+/*
+Problem
+Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. Gunakan regex untuk melatih kemampuan regex.
+
+Code
+*/
+function cariPelaku(str) {
+//   var count = 0;
+//   console.log(str.match(/abc/g));
+  var tampung = str.match(/abc/g);
+
+  return tampung.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
